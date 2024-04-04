03cdf7df60ab74ab99aa6caa0269de74763f982b
Szymon Jackiewicz <sz.jackiewicz@gmail.com>
2024-04-04 13:02:47 +0200

    Mocki

---
+++ src/constants/mocks.ts
@@ -0,0 +1,62 @@
+export const TEAM_DATA = [
+  {
+    id: 1,
+    name: 'Cezary Kraśniewski',
+    position: 'Team Leader',
+    photo: 'https://www.w3schools.com/howto/img_avatar.png',
+    email: 'cezary.krasniewski@wakacje.pl',
+  },
+  {
+    id: 2,
+    name: 'Szymon Jackiewicz',
+    position: 'Frontend Developer',
+    photo: 'https://www.w3schools.com/howto/img_avatar.png',
+    email: 'szymon.jackiewicz@wakacje.pl',
+  },
+  {
+    id: 3,
+    name: 'Paweł Sowiński',
+    position: 'Senior Frontend Developer',
+    photo: 'https://www.w3schools.com/howto/img_avatar.png',
+    email: 'pawel.sowinski@wakacje.pl',
+  },
+  {
+    id: 4,
+    name: 'Alan Wielguszewski',
+    position: 'Frontend Developer',
+    photo: 'https://www.w3schools.com/howto/img_avatar.png',
+    email: 'alan.wielguszewski@wakacje.pl',
+  },
+]
+
+export const TRAINING_GROUP = [
+  {
+    id: 1,
+    name: 'Jacek Szymkiewicz',
+    groupName: 'Frontend',
+  },
+  {
+    id: 2,
+    name: 'Brajan Koderski',
+    groupName: 'Frontend',
+  },
+  {
+    id: 3,
+    name: 'Jessika Reaktowa',
+    groupName: 'Frontend',
+  },
+  {
+    id: 4,
+    name: 'Jan Krzan',
+    groupName: 'Frontend',
+  },
+]
+
+// Dane do poszczególnych TABÓW
+
+export const FRONT_TABS_DATA = [
+  {
+    tabName: 'Analityka',
+    tabData: '',
+  },
+]
