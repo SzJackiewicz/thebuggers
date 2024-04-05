375bc1ecf376dffdc30af2dcb5f05026ed1756ba
gsreniawska <gabriela.sreniawska@wakacje.pl>
2024-04-05 17:14:02 +0200

    Add homePage

---
+++ src/components/Home/Home-2.tsx
@@ -0,0 +1,31 @@
+import { Feature, Section } from '@/lib/api/getHomePageData'
+
+export const Home2 = (feature: Feature[], section: Section) => {
+  return (
+    <div className='bg-gray-900 py-24 sm:py-32'>
+      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+        <div className='mx-auto max-w-2xl lg:mx-0'>
+          <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>{section.featuresHeader}</h2>
+        </div>
+        <div className='mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none'>
+          <dl className='grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3'>
+            {feature.map((feature) => (
+              <div
+                key={feature.featureTitle}
+                className='flex flex-col'
+              >
+                <dt className='text-base font-semibold leading-7 text-white'>
+                  <div className='mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-500'></div>
+                  {feature.featureTitle}
+                </dt>
+                <dd className='mt-1 flex flex-auto flex-col text-base leading-7 text-gray-300'>
+                  <p className='flex-auto'>{feature.featureDescription}</p>
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/lib/api/getHomePageData.tsx
@@ -0,0 +1,64 @@
+import { performRequest, PerformRequestParams } from '@/lib/datocms'
+
+const PAGE_CONTENT_QUERY = `
+query getHomePage ($locale: SiteLocale){
+  page(locale: $locale,filter: {id: {eq: "DixkwF4VRR-ogq2oz2mLgw"}}) {
+    id
+    sections {
+      ... on FeatureListSectionRecord {
+        id
+        displayOption
+        featuresHeader
+        feature {
+          featureTitle
+          featureDescription
+          iconname
+        }
+      }
+    }
+  }
+}
+  `
+export interface Feature {
+  featureTitle: string
+  featureDescription: string
+  iconname: string
+}
+
+export interface Section {
+  id: string
+  displayOption: string
+  featuresHeader: string
+  feature: Feature[]
+}
+
+interface Page {
+  id: string
+  sections: Section[]
+}
+
+interface Data {
+  page: Page
+}
+
+function getPageRequest(isEnabled: boolean, locale: string = 'pl_PL'): PerformRequestParams {
+  return {
+    query: PAGE_CONTENT_QUERY,
+    includeDrafts: isEnabled,
+    variables: {
+      locale: locale,
+    },
+  }
+}
+
+export async function getHomePageData(locale: string = 'pl_PL'): Promise<Data | undefined> {
+  try {
+    const pageRequest = getPageRequest(false)
+    const data = await performRequest<Data>(pageRequest)
+    console.log(data)
+    return data
+  } catch (error) {
+    console.error('Error fetching navigation data:', error)
+    return undefined
+  }
+}
