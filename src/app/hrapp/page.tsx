03b24eaa6d8441d890612a2e8fc1958422b785c7
Szymon Jackiewicz <sz.jackiewicz@gmail.com>
2024-04-06 00:33:36 +0200

    users

--- src/app/hrapp/page.tsx
+++ src/app/hrapp/page.tsx
@@ -3,5 +3,42 @@ import { getUsers } from '@/server/getUsers'
 export default async function HrApp() {
   const users = await getUsers()
   console.log({ users })
-  return <div>HrApp</div>
+  return (
+    <div>
+      {users?.map((user) => (
+        <div className='flex flex-col border-2 border-zinc-400 my-2 p-2'>
+          <label
+            className='text-md'
+            key={user.id}
+          >
+            {user.name}
+          </label>
+          <label
+            className='text-md'
+            key={user.id}
+          >
+            {user.surname}
+          </label>
+          <label
+            className='text-md'
+            key={user.id}
+          >
+            {user.email}
+          </label>
+          <label
+            className='text-md'
+            key={user.id}
+          >
+            {user.link}
+          </label>
+          <label
+            className='text-md'
+            key={user.id}
+          >
+            {user.about}
+          </label>
+        </div>
+      ))}
+    </div>
+  )
 }
