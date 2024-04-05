560edd6ce055d638183816c93deb196f043cedc4
gsreniawska <gabriela.sreniawska@wakacje.pl>
2024-04-05 17:11:04 +0200

    Add homePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
 import Image from 'next/image'
 
-export default function Home() {
-  return <main className='flex min-h-screen flex-col items-center justify-between p-24'>THEBUGGERS</main>
+import { getHomePageData } from '@/lib/api/getHomePageData'
+import { Home1 } from '@/components/Home/Home-1'
+import { Home2 } from '@/components/Home/Home-2'
+
+export default async function Home() {
+  const locale = 'en'
+  const data = await getHomePageData(locale)
+  const section = data?.page.sections[0]
+  if (!section) return
+
+  const feature = section.feature
+
+  const chooseComponent = (name: string) => {
+    console.log(name)
+    switch (name) {
+      case 'card':
+        return Home1(feature, section)
+      case 'grid':
+        return Home2(feature, section)
+      default:
+        return Home1(feature, section)
+    }
+  }
+
+  const ChosenComponent = chooseComponent(section.displayOption)
+
+  return <div>{ChosenComponent}</div>
 }
