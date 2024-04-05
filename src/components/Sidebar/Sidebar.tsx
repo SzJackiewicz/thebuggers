2b5005ffcf5884a1f4fd68ceefc4ccf0cf2e82c0
Szymon Jackiewicz <sz.jackiewicz@gmail.com>
2024-04-05 10:50:51 +0200

    poprawa nawigacji

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -45,14 +45,14 @@ const Sidebar = () => {
             </li>
             <li className='mt-auto'>
               <a
-                href='#'
+                href='/admin'
                 className='group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white'
               >
                 <Cog6ToothIcon
                   className='h-6 w-6 shrink-0'
                   aria-hidden='true'
                 />
-                Settings
+                Admin
               </a>
             </li>
           </ul>
