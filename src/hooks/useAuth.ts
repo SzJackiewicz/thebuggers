7e529eecd2a9b16ea9b01924418c8b10123a08b4
Pawel Sowinski <55237815+pawelsowinski93@users.noreply.github.com>
2024-04-05 10:54:26 +0200

    final logging in

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,32 +1,48 @@
-import { User } from "@/interfaces/User";
-import { useEffect, useState } from "react";
-import useCookie from "./useCookie";
+import { User } from '@/interfaces/User'
+import { useEffect, useState } from 'react'
+import useCookie from './useCookie'
 
 const hrUser: User = {
-  id: "924c3fb8-4529-4caa-8eec-6e4f8ce3131e",
-  fullName: "Simeon Graham",
+  id: '924c3fb8-4529-4caa-8eec-6e4f8ce3131e',
+  fullName: 'Simeon Graham',
   age: 28,
-  email: "simeon.graham@email.com",
-  departmentId: "123",
-  departmentName: "HR",
-};
+  email: 'simeon.graham@email.com',
+  departmentId: '123',
+  departmentName: 'HR',
+}
 
 const devUser: User = {
-  id: "464ba8b4-1c57-4262-b104-3b936bee592b",
-  fullName: "Braylon Griffin",
+  id: '464ba8b4-1c57-4262-b104-3b936bee592b',
+  fullName: 'Braylon Griffin',
   age: 31,
-  email: "braylon.griffin@email.com",
-  departmentId: "32",
-  departmentName: "IT",
-};
+  email: 'braylon.griffin@email.com',
+  departmentId: '32',
+  departmentName: 'IT',
+}
 
 export default function useAuth() {
-  const { set } = useCookie();
-  const [userData, setUserData] = useState<User>(hrUser);
+  const { set, remove } = useCookie()
+  const [userData, setUserData] = useState<User | null>(hrUser)
 
   useEffect(() => {
-    set("userData", userData);
-  }, [userData]);
+    if (userData) {
+      set('userData', userData)
+    } else {
+      remove('userData')
+    }
+
+    return () => {
+      remove('userData')
+    }
+  }, [userData])
+
+  const signOut = () => {
+    setUserData(null)
+  }
+
+  const signIn = (user: User) => {
+    setUserData(user)
+  }
 
   return {
     userData,
@@ -35,5 +51,7 @@ export default function useAuth() {
       hr: hrUser,
       dev: devUser,
     },
-  };
+    signOut,
+    signIn,
+  }
 }
