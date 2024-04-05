9cc0878f1eb4177a87ce934e5561eb0cfcd51f0c
Szymon Jackiewicz <sz.jackiewicz@gmail.com>
2024-04-06 00:56:12 +0200

    create user

--- src/server/createUser.ts
+++ src/server/createUser.ts
@@ -1,14 +1,26 @@
 'use server'
 import { prisma } from '@/lib/db'
 
-export async function createUser() {
+export async function createUser({ data }) {
   try {
-    const users = await prisma.user.findMany({
-      include: {
-        answers: true,
+    await prisma.user.create({
+      data: {
+        userId: data.userId,
+        name: data.name,
+        surname: data.surname,
+        email: data.email,
+        link: data.link,
+        about: data.about,
+        answers: {
+          create: [
+            {
+              key: data.key,
+              value: data.value,
+            },
+          ],
+        },
       },
     })
-    return users
   } catch (error) {
     console.error(error)
   }
