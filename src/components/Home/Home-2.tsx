c14a16a9ecc22cf1fd07b69e18eaa18cd60e4681
gsreniawska <gabriela.sreniawska@wakacje.pl>
2024-04-06 03:08:24 +0200

    map refactor

--- src/components/Home/Home-2.tsx
+++ src/components/Home/Home-2.tsx
@@ -3,8 +3,8 @@ import { getIconComponent } from '@/utils/getIconComponents'
 
 export const Home2 = (feature: Feature[], section: Section) => {
   return (
-    <div className='bg-gray-900 py-24 sm:py-32'>
-      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+    <div className='bg-gray-900 py-60'>
+      <div className='mx-auto max-w-7xl px-5 lg:px-10'>
         <div className='mx-auto max-w-2xl lg:mx-0'>
           <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>{section.featuresHeader}</h2>
         </div>
@@ -16,10 +16,12 @@ export const Home2 = (feature: Feature[], section: Section) => {
                 className='flex flex-col'
               >
                 <dt className='text-base font-semibold leading-7 text-white'>
-                  <div className='mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-500'>
-                    {getIconComponent(feature.iconname, 'shrink-0')}
+                  <div>
+                    <div className='mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-500'>
+                      {getIconComponent(feature.iconname, 'shrink-0')}
+                    </div>
+                    {feature.featureTitle}
                   </div>
-                  {feature.featureTitle}
                 </dt>
                 <dd className='mt-1 flex flex-auto flex-col text-base leading-7 text-gray-300'>
                   <p className='flex-auto'>{feature.featureDescription}</p>
---
+++ src/lib/api/getTodoList.tsx
@@ -0,0 +1,39 @@
+import { performRequest, PerformRequestParams } from '@/lib/datocms'
+const PAGE_CONTENT_QUERY = `
+query getHomePage{
+        allTodos {
+          todo
+        }
+      }
+    
+  `
+
+interface ToDo {
+  todo: string
+}
+
+interface Data {
+  allTodos: ToDo[]
+}
+
+function getPageRequest(isEnabled: boolean, locale: string = 'pl_PL'): PerformRequestParams {
+  return {
+    query: PAGE_CONTENT_QUERY,
+    includeDrafts: isEnabled,
+    variables: {
+      locale: locale,
+    },
+  }
+}
+
+export async function getToDoData(locale: string = 'pl_PL'): Promise<Data | undefined> {
+  try {
+    const pageRequest = getPageRequest(false, locale)
+    const data = await performRequest<Data>(pageRequest)
+    console.log(data)
+    return data
+  } catch (error) {
+    console.error('Error fetching navigation data:', error)
+    return undefined
+  }
+}
